8b2c0aaa52e91d9a0c7e1d0b4569b639fe661eb6
Florian FERBACH <florian@marmelab.com>
2016-09-06 14:35:43 +0200

    Add some kind of algorithm

--- src/connectfour/board.go
+++ src/connectfour/board.go
@@ -1,13 +1,21 @@
 package connectfour
 
 import (
+	"errors"
 	"strings"
 )
+
+const boardWidth int = 7
+const boardHeight int = 6
 
 const FirstPlayerChar string = "x"
 const SecondPlayerChar string = "o"
 
-type Board [6][7]uint8
+const MaxUint = ^uint(0)
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
+
+type Board [boardHeight][boardWidth]uint8
 
 func New(input string) Board {
 	lines := strings.Split(input, "\n")
@@ -23,4 +31,74 @@ func New(input string) Board {
 		}
 	}
 	return board
+}
+
+func (board Board) AddDisc(column, player uint8) (Board, error) {
+	dropped := false
+
+	for i := boardHeight - 1; i >= 0; i-- {
+		if board[i][column] == 0 {
+			dropped = true
+			board[i][column] = player
+			break
+		}
+	}
+
+	if !dropped {
+		return board, errors.New("Illegal move")
+	}
+
+	return board, nil
+}
+
+func (board Board) score(player uint8) int {
+
+	playerFourAligned, playerThreeAligned, playerTwoAligned := board.numberOfAlignedDiscs(player)
+
+	opponent := getOpponent(player)
+	_, _, opponentFourAligned := board.numberOfAlignedDiscs(opponent)
+
+	if opponentFourAligned > 0 {
+		return MinInt
+	}
+	return playerFourAligned*MaxInt + playerThreeAligned*100 + playerTwoAligned
+}
+
+func (board Board) numberOfAlignedDiscs(player uint8) (fourAligned, threeAligned, twoAligned int) {
+	// convolution algorithm
+
+	// vertical lines
+	for i := 0; i < boardWidth; i++ {
+		for j := 0; j < boardHeight-4; j++ {
+			// part := board[j : j+4][i]
+		}
+	}
+	return
+}
+
+func getOpponent(currentPlayer uint8) uint8 {
+	if currentPlayer == 1 {
+		return 2
+	}
+	return 1
+}
+
+func bestMove(board Board, player, depth int) (column, score int) {
+
+	if depth == 0 {
+		// compute column score
+	}
+	score = 0
+	depth--
+	// drop a disc on every column and compute possibility
+	for i := 0; i <= boardWidth; i++ {
+		// dropDisc(board, player, i)
+
+		// columnScore := getColumnScore(board, player, depth)
+		// if columnScore > score {
+		// 	score = columnScore
+		// 	column = i
+		// }
+	}
+	return
 }
